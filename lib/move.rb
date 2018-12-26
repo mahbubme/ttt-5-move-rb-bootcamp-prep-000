c8d3fc98ec8aa43782bf80a46742635dc247fd6b
Mahbubur Rahman <mail@mahbub.me>
2018-12-26 04:07:18 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ def input_to_index( input )
   return index = ( input.chomp.strip.to_i ) - 1
 end
 
-def move( board, index, player_character="X")
+def move( board, index, player_character="X" )
   board[index] = player_character
 end
 
